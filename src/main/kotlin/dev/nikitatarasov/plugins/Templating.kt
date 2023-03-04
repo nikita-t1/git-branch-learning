766590e07e6b97252badfebf64423cacf8a74cd5
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2023-03-04 20:26:18 +0100

    Removed 'Nice..'

--- src/main/kotlin/dev/nikitatarasov/plugins/Templating.kt
+++ src/main/kotlin/dev/nikitatarasov/plugins/Templating.kt
@@ -12,10 +12,6 @@ fun Application.configureTemplating() {
             call.respondHtml {
 
                 body {
-                    h1 { +"Nice... <script>alert('hi')</script>" }
-                    h1 { +"Nice2... <script>alert('hi')</script>" }
-                    h1 { +"Nice3... <script>alert('hi')</script>" }
-                    h1 { +"Nice4... <script>alert('hi')</script>" }
                     h1 { +"HTML" }
                     ul {
                         for (n in 1..10) {
